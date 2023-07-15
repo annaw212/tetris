e753d503c31421784dbe3064c3e495318fb4dc7c
Anna Elise Wong <anna.elise781@gmail.com>
2023-07-14 21:35:11 -0700

    Add project files.

---
+++ Tetris/LBlock.cs
@@ -0,0 +1,19 @@
+﻿namespace Tetris
+{
+    public class LBlock : Block
+    {
+        // Tile positions for 4 rotation states
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] { new(0, 2), new(1, 0), new(1, 1), new(1, 2) },
+            new Position[] { new(0, 1), new(1, 1), new(2, 1), new(2, 2) },
+            new Position[] { new(1, 0), new(1, 1), new(1, 2), new(2, 0) },
+            new Position[] { new(0, 0), new(1, 1), new(1, 1), new(2, 1) }
+        };
+
+        // Required properties
+        public override int Id => 3;
+        protected override Position StartOffset => new Position(0, 3);
+        protected override Position[][] Tiles => tiles;
+    }
+}
---
+++ Tetris/TBlock.cs
@@ -0,0 +1,19 @@
+﻿namespace Tetris
+{
+    public class TBlock : Block
+    {
+        // Tile positions for 4 rotation states
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] { new(0, 1), new(1, 0), new(1, 1), new(1, 2) },
+            new Position[] { new(0, 1), new(1, 1), new(1, 2), new(2, 1) },
+            new Position[] { new(1, 0), new(1, 1), new(1, 2), new(1, 1) },
+            new Position[] { new(0, 1), new(1, 0), new(1, 1), new(2, 1) }
+        };
+
+        // Required properties
+        public override int Id => 6;
+        protected override Position StartOffset => new Position(0, 3);
+        protected override Position[][] Tiles => tiles;
+    }
+}
